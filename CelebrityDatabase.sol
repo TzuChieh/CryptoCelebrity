cda762044fbb013085d90ffcfea3d98cad0aeb11
Tzu-Chieh Chang <b01502051@ntu.edu.tw>
2018-06-09 16:34:46 +0800

    added a simple database

---
+++ CelebrityDatabase.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.4.24;
+
+contract CelebrityDatabase
+{
+    struct Celebrity
+    {
+        string name;
+        uint   dna;
+        uint   reputation;
+        uint   power;
+    }
+
+    event CelebrityCreated(uint id, string name, uint dna);
+    
+    Celebrity[] public celebrities;
+
+    mapping (uint    => address) public celebrityToBoss;
+    mapping (address => uint   ) public bossCelebrityCount;
+
+    function _createCelebrity(string _name, uint _dna) internal
+    {
+        uint id = celebrities.push(Celebrity(_name, _dna, 0, 0)) - 1;
+        celebrityToBoss[id] = msg.sender;
+        bossCelebrityCount[msg.sender]++;
+
+        emit CelebrityCreated(id, _name, _dna);
+    }
+}
